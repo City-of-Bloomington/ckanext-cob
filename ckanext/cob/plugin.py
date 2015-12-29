465b3963e7ccfa90614e865f037e44a6050d38a2
Dan Hiester <hiesterd@bloomington.in.gov>
2015-12-29 10:46:55 -0500

    First draft of Browse By Topic on Homepage.

--- ckanext/cob/plugin.py
+++ ckanext/cob/plugin.py
@@ -1,9 +1,13 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+def groups():
+    # Return a list of groups
+    return toolkit.get_action('group_list')(data_dict={'all_fields': True})
 
 class CobPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -11,3 +15,7 @@ class CobPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'cob')
+
+    def get_helpers(self):
+        # Register cob_theme_* helper functions
+        return {'cob_theme_groups': groups}
